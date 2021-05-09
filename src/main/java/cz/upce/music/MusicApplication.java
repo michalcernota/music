6ef1cc10d3b2e57592ba2a07abbaa627bec3019c
Michal Černota <st52494@upce.cz>
2021-05-09 11:53:54 +0200

    Cors configuration

--- src/main/java/cz/upce/music/MusicApplication.java
+++ src/main/java/cz/upce/music/MusicApplication.java
@@ -1,10 +1,17 @@
 package cz.upce.music;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MusicApplication {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MusicApplication.class, args);
