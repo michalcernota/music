45346a04b855c6d5d546ea42584f64baca0cbd3d
Michal Černota <st52494@upce.cz>
2021-06-15 00:33:40 +0200

    insert admin edit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,5 @@
-/*
 INSERT INTO USERS (USERNAME, PASSWORD, USER_ROLE)
 SELECT 'admin',
        '$2a$10$PWs9jCQNvpo0DDK6./YP1.JxQxnTgiXsroKsZ6tDGzkWdWTSP9VJu', --BCrypt password admin
        'ROLE_ADMIN'
 WHERE NOT EXISTS (SELECT * FROM USERS WHERE USERNAME='admin' AND USER_ROLE='ROLE_ADMIN');
-*/
-INSERT INTO USERS (USERNAME, PASSWORD, USER_ROLE) values ('admin',
-                                                          '$2a$10$PWs9jCQNvpo0DDK6./YP1.JxQxnTgiXsroKsZ6tDGzkWdWTSP9VJu',
-                                                          'ROLE_ADMIN');
