ecb6e601f7bb7fc4bc77e4cecbd79953db74ac3d
Michal Černota <st52494@upce.cz>
2021-06-15 19:33:47 +0200

    Init data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,3 @@
-INSERT INTO USERS (USERNAME, PASSWORD, USER_ROLE)
-SELECT 'admin',
-       '$2a$10$PWs9jCQNvpo0DDK6./YP1.JxQxnTgiXsroKsZ6tDGzkWdWTSP9VJu', --BCrypt password admin
-       'ROLE_ADMIN'
-WHERE NOT EXISTS (SELECT * FROM USERS WHERE USERNAME='admin' AND USER_ROLE='ROLE_ADMIN');
+INSERT INTO USERS (USERNAME, PASSWORD, USER_ROLE) VALUES ('admin',
+                                                          '$2a$10$PWs9jCQNvpo0DDK6./YP1.JxQxnTgiXsroKsZ6tDGzkWdWTSP9VJu', --BCrypt password admin
+                                                          'ROLE_ADMIN');
